5b6cb98b0c856b8defc73be287cddf518b1584ed
Fabián Torres <nftorresc@gmail.com>
2023-11-26 15:36:16 -0500

    Ajustes necesarios

---
+++ src/app/character/character.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-character',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.css'
+})
+export class CharacterComponent {
+
+}
---
+++ src/app/characters/characters.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-characters',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './characters.component.html',
+  styleUrl: './characters.component.css'
+})
+export class CharactersComponent {
+
+}
